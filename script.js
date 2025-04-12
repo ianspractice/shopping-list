f4101ddab1aeb8ec86f4fcfbdedfa8a44718f88b
ianspractice <bodybody.ian@gmail.com>
2025-04-12 14:57:40 -0600

    Add and delete items

---
+++ script.js
@@ -0,0 +1,24 @@
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", addItem);
+
+function addItem() {
+  const value = input.value;
+  console.log(value);
+  input.value = "";
+  console.log(input.value);
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  span.textContent = value;
+  delBtn.textContent = "Delete";
+  ul.appendChild(li);
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
+  input.focus();
+}
